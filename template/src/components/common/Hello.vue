e39d43b0ea4ddd9bdd90fd7fcc9b63d87e98bc2b
fanxmistic <qjx0602@gmail.com>
2017-05-02 21:00:10 +0800

    change hello

--- template/src/components/common/Hello.vue
+++ template/src/components/common/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msgs }}</h1>
   </div>
 </template>
 
@@ -9,7 +9,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Offline App.'
+      msgs: 'Welcome to Your Offline App.'
     }
   }
 }
